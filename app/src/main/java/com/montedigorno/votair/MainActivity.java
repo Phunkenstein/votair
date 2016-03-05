1e8d90cb1a7bfc33c41a37de4548aeeffc0f6dea
Chris Harmon <harmonjchris@gmail.com>
2016-03-05 10:38:44 -0600

    Added App Engine Backend with GCM privileges.

--- app/src/main/java/com/montedigorno/votair/MainActivity.java
+++ app/src/main/java/com/montedigorno/votair/MainActivity.java
@@ -38,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        new GcmRegistrationAsyncTask(this).execute();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
