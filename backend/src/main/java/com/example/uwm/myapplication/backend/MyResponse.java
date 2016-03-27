26d7caed4d93582ffd15cd134f27b7dfa903044c
Chris Harmon <harmonjchris@gmail.com>
2016-03-27 18:46:42 -0500

    making the server base a backend api

---
+++ backend/src/main/java/com/example/uwm/myapplication/backend/MyResponse.java
@@ -0,0 +1,17 @@
+package com.example.uwm.myapplication.backend;
+
+/**
+ * Created by Chris Harmon on 3/27/2016.
+ */
+public class MyResponse {
+    private String myData;
+
+    public String getData() {
+        return myData;
+    }
+
+
+    public void setData(String data) {
+        myData = data;
+    }
+}
