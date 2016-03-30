9d7754ab8badf7073fb6a0fbf39fac20d6861d64
brianhildebrand <brianhenryhildebrand@gmail.com>
2016-03-30 18:59:19 -0500

    Adding data models and fragment_profile.

---
+++ app/src/main/java/com/montedigorno/votair/models/ElectionModel.java
@@ -0,0 +1,13 @@
+package com.montedigorno.votair.models;
+
+import com.google.api.client.util.DateTime;
+
+/**
+ * Created by brianhildebrand on 3/29/16.
+ */
+public class ElectionModel {
+
+    private String electionName;
+    private DateTime electionDate;
+
+}
